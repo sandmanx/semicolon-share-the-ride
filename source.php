81290f2d70c6185b42e68ae9638e7ddb403d2712
Arpitrf <arpitbahety@hotmail.com>
2016-09-11 01:05:25 +0530

    Update source.php

--- source.php
+++ source.php
@@ -10,16 +10,24 @@
 			<td><input type="text" name="name"></td>
 			</tr>
 			<tr>
-			<td>Email</td>
-			<td><input type="text" name="email"></td>
-			</tr>
-			<tr>
 			<td>username</td>
 			<td><input type="text" name="username"></td>
 			</tr>
 			<tr>
+			<td>Email</td>
+			<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
 			<td>Password</td>
 			<td><input type="password" name="pass"></td>
+			</tr>
+			<tr>
+			<td>Confirm Password</td>
+			<td><input type="password" name="c_pass"></td>
+			</tr>
+			<tr>
+			<td>Contact Number</td>
+			<td><input type="integer" name="c_no"></td>
 			</tr>
 			<tr>
 			<td><input type="submit" name="submit" value="submit"></td>
