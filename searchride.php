f9f5728037bc6b1f0d231bfa9deab0a7cf039a48
shaken-notstirred <iit2015088@iiita.ac.in>
2016-09-11 02:23:50 +0530

    Add files via upload

---
+++ searchride.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title> Create Ride</title>
+	</head>
+	<body>
+		<table cellspacing="15px" border="0px">
+		<form action="cr.php" method="POST">
+			<tr>
+			<td>Source</td>
+			<td><input type="text" name="source"></td>
+			</tr>
+			<tr>
+			<td>Destination</td>
+			<td><input type="text" name="destination"></td>
+			</tr>
+			<td><input type="submit" name="Search Ride" value="Search Ride"></td>
+			</tr>
+
+	</body>
+
+</html>
